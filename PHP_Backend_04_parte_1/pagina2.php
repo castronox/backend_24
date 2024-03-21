863cb07749d9104d29b5fde7faead08e2bfc07b9
[castronox] <[castronox@outlook.es]>
2024-03-21 01:15:18 +0100

    Creación de pagina 2 para el ejemplo de sesión.

---
+++ PHP_Backend_04_parte_1/pagina2.php
@@ -0,0 +1,18 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documento PHP</title>
+</head>
+<body>
+    <h1>Ejemplo de sesiones</h1>
+    <p>Veamos si el dato sigue guardado:
+    <b><?= $_SESSION['nombre'] ?? 'El dato no existe' ?></b>
+    </p>
+    <a href="variables_sesion.php">Página 1</a>
+
+
+</body>
+</html>
