0defa0548cf28c18edc1b12e2dffe45911218e2a
castronox <castronox@outlook.es>
2024-03-21 19:33:01 +0100

    Operadores de incremento y decremento.

---
+++ PHP_basico_Backend_05_parte_2/operadores_de_incr_decr.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Incremento y drecremento PHP</title>
+</head>
+<body>
+    
+    <?php   
+    $a = 10;
+    
+    // --->  Incremento
+    
+    echo $a++;      // 10
+    echo '<br>';
+    
+    echo ++$a;      //  12
+    echo '<br>';
+    
+    
+    // --->  Decremento
+    $b = 10;
+    
+    
+    echo --$b;  //9
+    echo '<br>';
+    
+    echo $b--;  //9
+    
+    
+    
+    
+    
+    
+    ?>
+
+
+</body>
+</html>
