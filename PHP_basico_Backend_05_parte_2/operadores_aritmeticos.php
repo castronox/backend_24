191d7919aab3c2971da57b6817b45610e2b0029d
castronox <castronox@outlook.es>
2024-03-21 19:13:04 +0100

    Operadores aritméticos Ejemplo 1

---
+++ PHP_basico_Backend_05_parte_2/operadores_aritmeticos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores aritméticos</title>
+</head>
+<body>
+    
+    <?php   
+      $a = 10;      
+      $b = 5;      
+      
+      echo -$a.'<br>';      //-10
+      echo $a + $b.'<br>';  //15
+      echo $a - $b.'<br>';  //5
+      echo $a * $b.'<br>';  //50
+      echo $a / $b.'<br>';  //2
+      echo $a % $b.'<br>';  //0
+      echo $a ** $b.'<br>'; //100000
+    
+      
+      //Operador NO aritético
+      //No obstante se puede utilizar con numeros.
+      echo $a.$b.'<br>'; //105
+    ?>
+
+
+</body>
+</html>
