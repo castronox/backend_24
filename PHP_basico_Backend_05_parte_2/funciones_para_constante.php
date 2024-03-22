8a7e952646ad6db1cffce41897f0968dec38e81b
castronox <castronox@outlook.es>
2024-03-22 18:31:44 +0100

    Funciones para constantes PHP

---
+++ PHP_basico_Backend_05_parte_2/funciones_para_constante.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DConstantes en PHP</title>
+</head>
+<body>
+
+	<h1>Constantes</h1>
+	
+	<p>La función defined() sirve para definir constantes.</p>
+	
+	<p>La función defined sirve para comprobar si una constante ha sido definida previamente.</p>
+    
+    <?php   
+        
+    echo defined('DEBUG') ? '<p>Está definida</p>' : '<p>No está definida</p>';
+    
+    define ('DEBUG', false);
+    
+    echo defined('DEBUG') ? '<p>Está definida.</p>' : '<p>Noestá definida</p>';
+    
+    ?>
+
+
+</body>
+</html>
