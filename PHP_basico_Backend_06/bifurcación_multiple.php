bfa11396e85c6b5ef6994620568bfc7464c72b50
castronox <castronox@outlook.es>
2024-03-24 15:17:48 +0100

    Bifurcaciones Múltiples.

--- PHP_basico_Backend_06/bifurcación_multiple.php
+++ PHP_basico_Backend_06/bifurcación_multiple.php
@@ -3,12 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento PHP</title>
+    <title>Bifurcación múltiple PHP</title>
 </head>
 <body>
     
-    <?php   
+    <h1>Bifurcación múltiple o switch/case en PHP</h1>
     
+    
+    <?php   
+    $nota = 8.25;
+    
+    switch ((int) $nota){
+        case 0: echo "Supenso";
+                break;
+        case 1: echo "Supenso";
+                break;
+        
+        case 2: echo "Supenso";
+                break;
+        
+        case 3: echo "Supenso";
+                break;
+                
+        case 4: echo "Suspenso";
+                break;
+
+        case 5: echo "Aprovado";
+        break;
+        
+        case 6: echo "Aprovado";
+        break;
+        
+        case 7: echo "Notable";
+        break;
+        
+        case 8: echo "Notable";
+        break;
+        
+        case 9: echo "Excelente";
+        break;
+        
+        case 10: echo "Excelente";
+        break;
+        
+        default: echo "Nota incorrecta";
+        
+    }
     
     ?>
 
