2070b2b008c8a39e60e9c1bab57264e17f2bf979
castronox <castronox@outlook.es>
2024-03-24 15:10:49 +0100

    Bifurcación doble en PHP

---
+++ PHP_basico_Backend_06/bifurcacion_doble.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bifurcación  PHP</title>
+</head>
+<body>
+    <h1>Bifurcación doble en PHP</h1>
+    <?php   
+    
+    $nota = 7; //Modifica este valor.
+    
+    // Bifurcación doble (if-else).
+    
+    if($nota<5){
+        
+        echo "<p>Has suspendido</p>";
+    }else{
+            
+        echo "<p>Has aprobado. </p>";
+        }
+        
+        echo "Fin del programa";
+    
+    ?>
+    
+    
+    
+
+
+</body>
+</html>
