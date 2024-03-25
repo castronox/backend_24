564bdb77b3f95c622a4fc7c754bc5234801c93e4
castronox <castronox@outlook.es>
2024-03-25 19:03:18 +0100

    tipos de bifurcaciones por rango y omitiendo break.

--- PHP_basico_Backend_06/bifurcacion_multiple_2.php
+++ PHP_basico_Backend_06/bifurcacion_multiple_2.php
@@ -3,11 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento PHP</title>
+    <title>Bifurcación multiple p.2 PHP</title>
 </head>
 <body>
+
+<h1>Bifurcación multiple omitiendo BREAK</h1>
     
     <?php   
+    $nota = 5.5; 
+    
+    
+    switch ((int) $nota){
+        
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4: echo "suspenso";
+                break;
+        case 5:
+        case 6: echo "aprovado";
+                break;
+        case 7:
+        case 8: echo "notable";
+                break;
+        case 9:
+        case 10:echo "excelente";
+                break;
+        default: echo "nota incorrecta";
+    }
     
     
     ?>
---
+++ PHP_basico_Backend_06/bifurcacion_multiple_3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bifurcación múltiple p.3 PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+    $nota = 9.25;
+    
+    switch (true){
+        
+        case $nota <0 || $nota>10: echo "nota incorrecta";
+                       break;
+        case $nota <5: echo "suspenso";
+                       break;
+        case $nota <7: echo "notable";
+                       break;                      
+        default: echo "excelente";
+    }
+    
+    ?>
+
+
+</body>
+</html>
