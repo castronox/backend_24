1cf27be3d6b8dd55db1bedd853d867393838b119
castronox <castronox@outlook.es>
2024-03-26 13:02:06 +0100

    Algunas funciones para tratar con Strings en PHP

---
+++ PHP_basico_Backend_06/strings/funciones_strings.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funciones para string en PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+    #Cadena de texto 
+    $texto = "HOLA MUNDO!";
+    
+    echo $texto . '<br>';
+    
+    echo strtolower($texto). '<br>';
+    echo strrev($texto). '<br>';
+    echo md5($texto). '<br>';
+    
+    
+    #La variable original no cambia.
+    
+    echo $texto . '<br>';
+    
+    #Si queremos que cambie : 
+    
+    $texto = strrev($texto);
+    echo $texto . '<br>';
+    
+    
+    ?>
+
+
+</body>
+</html>
