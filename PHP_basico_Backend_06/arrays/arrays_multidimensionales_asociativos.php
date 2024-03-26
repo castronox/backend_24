824455c21a0f1d5bc795a3c8d33a8ff5337491fc
castronox <castronox@outlook.es>
2024-03-26 11:50:45 +0100

    Array multidimensionales asociativos, consultas y impresión utilizando bucles.

---
+++ PHP_basico_Backend_06/arrays/arrays_multidimensionales_asociativos.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays Multidim. Asociativos en PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+    #Arrays Multidimensionales asociativos.
+    
+   
+    
+    
+    // Matriz 2d Indexada y Asociativa.
+    // Para acceder a una posición: $coches[index]['clave'];
+    
+    $coches = [
+            
+        ['marca' => 'Seat'     ,  'modelo' =>     'León'],
+        ['marca' => 'Ford'     ,  'modelo' =>    'Focus'],
+        ['marca' => 'Peugeot'  ,  'modelo' =>  'Partner']
+    
+        
+              ];
+    
+    
+    # Para recuperar un coche
+    print_r($coches[1]);  // Array Ford Focus
+    
+    # Para recuperar un valor en concreto
+    echo '<p>' . $coches[2]['modelo']. '</p>' ; // 'Partner'
+    
+    # Para imprimir los datos del array multidimensional asociativo con foreach.
+    
+    foreach ($coches as $coche){
+        echo "<p>Coche</p>";
+        
+        foreach ($coche as $clave => $valor){
+            echo "$clave: $valor<br>";
+        }
+    }
+    
+    ?>
+
+
+</body>
+</html>
