e2479400f00626d1c08b9360435fbd427f36fc0b
castronox <castronox@outlook.es>
2024-03-26 14:24:43 +0100

    Ejercicio 6 en php

---
+++ PHP_basico_Backend_06/ejercicios/ejercicio_6.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 6 PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+    
+    $limite = 11;
+    
+    for ($i = 1; $i < $limite; $i++) {
+        
+        echo '<h3>Tabla del ' . $i. '<br></h3>';
+        for( $j = 0; $j < 10; $j++ ){
+            
+            
+             echo "El resultado de $i x $j es: ". $i * $j .'<br>';
+        }
+    }
+    ?>
+
+
+</body>
+</html>
