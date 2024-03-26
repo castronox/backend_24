e7c81b00a2d030247074979485f3a29559a2208a
castronox <castronox@outlook.es>
2024-03-26 13:12:36 +0100

    Eejercicio_1 resuelto.

---
+++ PHP_basico_Backend_06/ejercicios/ejercicio_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 en PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+    for ($i = 0; $i <101; $i++) {
+        Echo "Esta es la iteración : $i <br>";
+    }
+    
+    ?>
+
+
+</body>
+</html>
