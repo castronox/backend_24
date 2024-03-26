7cd350acae74da9c265dcac04d22151ed8eec2b6
castronox <castronox@outlook.es>
2024-03-26 10:31:34 +0100

    Arrays asociativos en PHP

---
+++ PHP_basico_Backend_06/arrays/arrays_asociativos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays asociativos PHP</title>
+</head>
+<body>
+    
+    <?php   
+    $persona = []; #Crea un array vacío
+    $persona['nombre'] = 'Pepe';        # Añade la clave 'nombre'.
+    $persona['apellido'] = 'Lopez';     # Añade la clave 'apellido'.
+    $perosna['edad'] = 22;              # Añade la clave 'edad'.
+    
+    echo "<pre>";
+    print_r($persona);
+    echo "</pre>";
+    
+    //Accediendo a las posiciones
+    $persona['nombre'] = 'Marta';               # Escritura
+    echo '<p>' .$persona['apellido']. '</p>';   # Lectura
+    
+    echo "<pre>";
+    print_r($persona);
+    echo "</pre>";
+    ?>
+
+
+</body>
+</html>
