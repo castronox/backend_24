25569a39269ff8965ddf6535fc6d60f3ffcfbcd0
castronox <castronox@outlook.es>
2024-03-26 10:32:14 +0100

    Correción de error tipográfico

---
+++ PHP_basico_Backend_06/arrays/añadir_posiciones_array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Añadir posiciones a un array PHP</title>
+</head>
+<body>
+    
+    <?php   
+    echo "<h2> Nuevas posiciones en un array PHP</h2>";
+    
+    $lista = [10, 20]; // Posiciones 0 y 1
+    $lista[5] = 50; //Nueva Posición 5
+    
+    echo "<pre>";
+    var_dump($lista);
+    echo "</pre>";
+    
+    ?>
+
+
+</body>
+</html>
