b8f92360b70d1f3f387231b543fd0270fa9a3650
castronox <castronox@outlook.es>
2024-03-26 13:14:49 +0100

    Ejercicios de bucles en PHP

---
+++ PHP_basico_Backend_06/ejercicios/ejercicio_3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 en PHP</title>
+</head>
+<body>
+    
+    <?php  
+    
+    echo "<h1>Ejercicio 1 Backend _ 06</h1>";
+    
+    for ($i = 0; $i <101; $i++) {
+        Echo "Esta es la iteración : $i <br>";
+    }
+    
+    ?>
+
+
+</body>
+</html>
