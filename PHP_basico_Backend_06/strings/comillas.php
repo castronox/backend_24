97c97876efa7ce0a13ae13f750fc41fbedb2da2f
castronox <castronox@outlook.es>
2024-03-26 12:48:39 +0100

    Entrecomillado / concatenación de string en PHP

---
+++ PHP_basico_Backend_06/strings/comillas.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comillas dobles y simples en PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+        $a = "Patatas";
+        $b = 100;
+    
+    
+    
+            #Entrecomillado doble
+            $texto = "Hay $b $a en la cesta. \n";
+                
+                echo $texto . '<br>';
+            
+            
+            
+            #Entrecomillado simple (ERROR)
+            $texto = 'Hay $b $a en la cesta. \n';
+                
+                echo $texto . '<br>';
+            
+            
+            
+            //Entrecomillado simple
+            $texto = 'Hay' . $b . ' ' . $a . ' en la cesta';
+                
+                echo $texto. '<br>';
+    
+    ?>
+
+
+</body>
+</html>
