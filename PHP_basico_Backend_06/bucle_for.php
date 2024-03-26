3e890b7f30da76ab6fafc14b8391096ef826c3c1
castronox <castronox@outlook.es>
2024-03-26 09:20:50 +0100

    Bucles for ejemplos de explicación

---
+++ PHP_basico_Backend_06/bucle_for.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bucle FOR PHP</title>
+</head>
+<body>
+
+
+<h1> Ejemplo de bucle for simple</h1>
+    
+    <?php   
+    
+    for($i = 5; $i<=15; $i++){
+        echo $i. '<br>'; // Imprime $i
+    }
+    
+    ?>
+
+
+</body>
+</html>
---
+++ PHP_basico_Backend_06/bucle_for_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bucle de incrementos múltiples en PHP</title>
+</head>
+<body>
+    
+    <?php   
+    
+    echo '<h2>Bucle FOR para realizar inicializaciones e incrementos múltiples</h2>';
+    
+    for($i = 0, $j=10;   $i != $j; $i++, $j--){echo "$i - $j <br>";}
+    ?>
+
+
+</body>
+</html>
