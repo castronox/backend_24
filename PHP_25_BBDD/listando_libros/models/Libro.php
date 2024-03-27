9a624f8b7d64644ead2c33f2ee930f393bdb97b5
castronox <castronox@outlook.es>
2024-03-27 19:16:22 +0100

    Creacion de recuperarLibros.php para hacer una consulta y listar en un array con fetch_object los datos.

--- PHP_25_BBDD/listando_libros/models/Libro.php
+++ PHP_25_BBDD/listando_libros/models/Libro.php
@@ -1,7 +1,16 @@
 <?php
-namespace PHP_25_BBDD\listando_libros\models;
+
 
 class Libro
 {
+    #No indicaré propiedades, las creará dinámicamente el método fetch_object()
+    #a partir de los campos recuperados de la consulta
+    
+    #No indicaré el constructor , da problemas con fetch_object()
+    
+    #Implementaré el método toString() solamente para demostrar que funciona
+    public function __toString(){
+        return "$this->titulo, de $this->autor. Editorial $this->editorial.";
+    }
 }
 
