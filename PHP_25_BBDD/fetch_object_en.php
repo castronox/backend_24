db1a37485c512dbbf3be02ca03b14fe1d08a6177
castronox <castronox@outlook.es>
2024-03-27 12:50:00 +0100

    Coreccion de sintaxis en el ejemplo.

--- PHP_25_BBDD/fetch_object_en.php
+++ PHP_25_BBDD/fetch_object_en.php
@@ -3,12 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento PHP</title>
+    <title>Fetch Oject en PHP</title>
 </head>
 <body>
     
     <?php   
     
+    $conexion = new mysqli('localhost', 'root','','biblioteca');
+    $conexion ->set_charset('utf8');
+    
+    $consulta= "SELECT id, titulo, autor FROM libros ORDER BY id ASC";
+    $resultado = $conexion->query($consulta);
+    
+    while ($libro = $resultado->fetch_object()){
+        
+        echo "$libro->id - $libro->titulo, de $libro->autor.<br/>";
+    }
+    
+    $resultado -> free();
     
     ?>
 
