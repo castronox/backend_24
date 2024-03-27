c28953ed72b481efd648fcad429853352c728fae
castronox <castronox@outlook.es>
2024-03-27 16:27:56 +0100

    Creaciones y definición de constantes para la BBDD orientado a MVC en PHP

--- PHP_25_BBDD/listando_libros/config/config.php
+++ PHP_25_BBDD/listando_libros/config/config.php
@@ -5,8 +5,15 @@
 
 define('AUTOLOAD_DIRECTORIES' , ['models']);
 
-#
+#Parametros de configuración de la BBDD
+
+define('DB_HOST', 'localhost');     #HOST
+define('DB_USER', 'root');          #USUARIO
+define('DB_PASS', '');              #CONTRASEÑA
+define('DB_NAME', 'biblioteca');    #NOMBRE_BBDD
+define('DB_PORT', 3306);            #PUERTO
+define('DB_PASS', 'utf8');          #CODIFICACIÓN UTF-8
     
-    
-    ?>
+
+?>
 
