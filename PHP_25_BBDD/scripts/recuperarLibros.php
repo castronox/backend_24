c5d798b5130a8e5aeb9fa7eb693f79950f6719f1
castronox <castronox@outlook.es>
2024-03-27 19:18:38 +0100

    Creamos la vista del array de la consulta de libros

--- PHP_25_BBDD/scripts/recuperarLibros.php
+++ PHP_25_BBDD/scripts/recuperarLibros.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento PHP</title>
-</head>
-<body>
-    
+
     <?php   
+    #Lanza la consulta
     
+    $resultado = $conexion->query("SELECT * FROM libros");
     
+    #Prepara una lista de libros
+    $libros = [];
+    
+    while($libro = $resultado->fetch_object('libro')){
+        $libros[] = $libro; # Y lo mete en la lista
+    }
+    
+    $resultado->free(); #Liberamos memoria
+    
+    #En este punto, hemos convrtido el resultado de la consulta en una lista de libros.
     ?>
-
-
-</body>
-</html>
