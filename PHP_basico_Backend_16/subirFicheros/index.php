749448c176bbb3663cb08f3eb4bc2439fac71dc1
castronox <castronox@outlook.es>
2024-04-08 23:38:42 +0200

    Subida de ficheros e imagenes con PHP pg 33 hacer proximamente

---
+++ PHP_basico_Backend_16/subirFicheros/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subir Ficheros en PHP</title>
+</head>
+<body>
+    
+    
+    <form method = "POST" enctype="multipart/form-data" action="upload.php">
+    
+    <label>Sube tu imágen de perfil:</label>
+    <br>
+    <input type = "hidden" name = "MAX_FILE_SIZE" value = "1000000">
+    <input type = "file" accept="image/*" name="fichero">  
+    <br>
+    <input type="submit" value="Enviar">  
+    
+    </form>
+
+</body>
+</html>
---
+++ PHP_basico_Backend_16/subirFicheros/upload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documento PHP</title>
+</head>
+<body>
+    <h2>Datos recibidos del fichero en el servidor:</h2>
+    <pre>
+    <?php   
+    var_dump($_FILES);
+    
+    ?>
+</pre>
+
+</body>
+</html>
