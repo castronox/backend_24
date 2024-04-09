5048fa19ec9116a6b36ed95c1d24b6da18de744c
castronox <castronox@outlook.es>
2024-04-09 20:06:32 +0200

    Creacion de la clase FileList en PHP

---
+++ PHP_basico_Backend_18/FileList/index.php
@@ -0,0 +1,22 @@
+<?php   
+
+# Carga la libreria
+include 'libraries/FileList.php';
+
+
+# Usaremos el método estático FileList::get()
+# en el primer parámetro indicamos la carpeta, si no 
+# lo indicamos usará el directorio actual
+# el segundo parámetro indica tipos y debe ser una expresión regular,
+# si no lo indicamos listará todo lo que encuentre
+$archivos = FileList::get('imagenes', '/\.(gif|jpe?g|png)$/i');
+
+# Genera las figuras de la galería
+foreach($archivos as $archivo){ ?>
+
+<figure>
+<img src='<?= $archivo ?>'>
+<figcaption><?= $archivo ?></figcaption>
+</figure>
+
+<?php } ?>
