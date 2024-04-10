9c961d7cfb8601d9af974ed458b33d4393e7c1d7
castronox <castronox@outlook.es>
2024-04-10 07:08:39 +0200

    File put contents en php, COMENTA CUANTOS BYTES SED ESCRIBIERON

--- PHP_basico_Backend_19/file_put_contents/file_put_contents.php
+++ PHP_basico_Backend_19/file_put_contents/file_put_contents.php
@@ -2,5 +2,20 @@
     
 
 # Solicitamos la URL
+$texto = file_get_contents('https://biblioteca.robertsallent.com');
+
+if (!$texto)
+    throw new Exception("No se pudo leer");
+
+    # Escribo lo recuperado en un fichero
+    # Abre escribe y cierra en un solo paso
+    
+    $done = file_put_contents('copia.txt', $texto);
+    
+    if($done === false)
+        throw new Exception("Nose pudo escribir");
+    
+        echo "Escritura del fichero finalizada, ";
+        echo "se escribieron $done bytes.";
     
     ?>
