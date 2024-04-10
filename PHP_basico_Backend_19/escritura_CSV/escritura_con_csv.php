19b9322269a4c3beaead39b1ac1df9293c77c38a
castronox <castronox@outlook.es>
2024-04-10 07:14:46 +0200

    Introduccion de datos en Formato CSV

---
+++ PHP_basico_Backend_19/escritura_CSV/escritura_con_csv.php
@@ -0,0 +1,23 @@
+
+<?php   
+    $coches = [
+        
+        ['Seat','Ibiza','11000'],
+        ['Ford','Raptor','76000'],
+        ['Cupra','Formentor','19350']
+    ];
+    
+    
+    # Abre el fichero para su escritura
+    $fichero = fopen('datos/coches.csv', 'w');
+    
+    # Vuelca el array al fichero CSV
+    foreach($coches as $coche)
+        fputcsv($fichero, $coche);
+        
+        fclose($fichero); # Cierra el fichero.
+        
+        echo "Volcado de datos realizado.";
+    
+    
+    ?>
