0c17551bd3fd3a24ed5857f6169ae9a0321f42f8
castronox <castronox@outlook.es>
2024-04-11 07:39:16 +0200

    Envío de Mail y Mail con HTML

---
+++ PHP_basico_Backend_24/funcion_mail.php
@@ -0,0 +1,18 @@
+<?php   
+    
+
+# Preparacion de los parametros a pasarle la funcion mail()
+
+$to             = "castronox@outlook.es";       # Receptor
+$subject        = "Test de envío local";        # Asunto
+$message        = "Hola mundo!";
+
+# Cabeceras adicionales
+
+$headers  = "To <$to>\r\n";
+
+$headers .="From: LOCALHOST <cristian@misitio.com>\r\n";
+
+# Llamamos a la funcion mail con los parámetros adecuados
+echo mail($to, $subject, $message, $headers) ? "ENVIADO" : "NO ENVIADO";
+            ?>
---
+++ PHP_basico_Backend_24/mail_con_html.php
@@ -0,0 +1,27 @@
+
+<?php   
+
+$to     = "castronox@outlook.es"; # Receptor
+$subject= "Test de envío local" ;
+$headers = "MIME-Version: 1.0\r\n";
+$headers .="Content-type: text/html; charset=utf-8\r\n";
+$headers .="From: NOREPLY <noreply@cristian.com>\r\n";
+
+#Mensaje en HTML (Uso de sintaxis HEREDOC que es mas sencilla para esto)
+
+$message= <<<EOD
+<html lang="es">
+    <head>
+        <title>Test de envío de mail</title>
+</head>
+<body>
+<h3>Test </h3>
+<p>Hola mundo</p>
+<img src ='https://juegayestudia.com/images/samples/logomenu.png'>
+<a href='https://juegayestudia.com'>Click-me</a>
+</body>
+</html>
+EOD;
+echo mail($to, $subject, $message, $headers)? "ENVIADO" : "NO ENVIADO";
+    
+    ?>
