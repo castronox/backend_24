66e3590c5f5b1954aeb92d07bef5135bd151f305
castronox <castronox@outlook.es>
2024-05-06 08:38:51 +0200

    Grabar y recuperar información del web storage

---
+++ PHP_basico_Backend_22/storage/recuperar_webStorage.php
@@ -0,0 +1,13 @@
+<h1>Recuperando información de Web Storage</h1>
+
+<p>Recuperando datos.</p>
+
+<script>
+document.write(sessionStorage.nombre);
+document.write(' - ');
+document.write(localStorage.centro);
+</script>
+
+
+<p>Cierra la pestaña o el navegador y vuelve a abrir esta página.
+Comprueba que se borra la información de sesión pero no la local.</p>
---
+++ PHP_basico_Backend_22/storage/webStorage.php
@@ -0,0 +1,13 @@
+<h1>WebStorage en PHP</h1>
+
+<p>Guardando datos...<p>
+
+<script>
+// Almacenamiento de sesión.
+sessionStorage.nombre = 'Cristian';
+
+// Almacenamiento local persistente
+localStorage.centro = 'Actihumá Terrassa';
+</script>
+
+<p> Se ha guardado la información, compruebalo en tu navegador con f12.</p>
